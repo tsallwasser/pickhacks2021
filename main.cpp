a35301cb519bab381cfc90a39fec1597c9eefd48
Timothy Sallwasser <tsallwasser@gmail.com>
2021-04-10 11:40:55 -0500

    lets try this

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Programmer: Timothy Sallwasser
+//School: Missouri University of Science and Technology
+//Project: Pickhacks 2021
+//Filename: main.cpp
+
+#include <iostream>
+#include "main.h"
+using namespace std;
+int main()
+{
+
+	return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//Programmer: Timothy Sallwasser
+//School: Missouri University of Science and Technology
+//Project: Pickhacks 2021
+//Filename: main.h
+
+#include <iostream>
+#ifndef MAIN_H
+#define MAIN_H
+using namespace std;
+
+
+
+
+#endif
